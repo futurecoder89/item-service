53d2d946a09424e1530e89fed36d6cb8bf052f9b
paul <leebosuk89@gmail.com>
2023-11-13 21:01:54 +0900

    23-11-13

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,10 +6,8 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 ///basic/items 경로로오면 items메서드가 작동함 그러면 모델에 items라고 컬렉션이 담김. 그후 basic/items경로를 리턴
@@ -38,11 +36,84 @@ public class BasicItemController {
     public String addForm() {
         return "basic/addForm";
     }
-    @PostMapping("/add")   //addForm에 method=post이기때문에 이게 호출됨
-    public String save() {
-        return "basic/addForm";
-    }
+    //@PostMapping("/add")   //addForm에 method=post이기때문에 이게 호출됨
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model) {
 
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+    //@PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item ) {
+
+
+        itemRepository.save(item);
+
+        //model.addAttribute("item", item); //자동 추가, 생략 가능
+
+        return "basic/item";
+    }
+    //@PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+
+        //@ModelAttribute("item")에서
+        // ("item)을 생략한다면 클래스명인
+        // Item에서 첫글자인 i를 소문자로
+        // 전환해서 모델어트리뷰트에 넣어준다
+        itemRepository.save(item);
+
+        //model.addAttribute("item", item); //자동 추가, 생략 가능
+
+        return "basic/item";
+    }
+    //@PostMapping("/add")
+    public String addItemV4(Item item) {
+        itemRepository.save(item);
+
+        //model.addAttribute("item", item); //자동 추가, 생략 가능
+
+        return "basic/item";
+    }
+    //@PostMapping("/add")
+    public String addItemV5(Item item) {
+        itemRepository.save(item);
+        return "redirect:/basic/items/" + item.getId(); //PRG 적용
+
+
+    }
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}"; //url 노출문제 해결한 버전
+
+
+    }
+    //상품 수정
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+    //상품 수정 저장버튼 action
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item){
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
     /**
      * 테스트용 데이터 추가
      */
